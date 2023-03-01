a6138c2b8f84a53519e1d784ce4026f91194720a
johan <johan.kasri@icloud.com>
2023-03-01 16:32:08 +0100

    mailer ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,45 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', name: 'app_home')]
+    public function index(MailerInterface $mailer,Request $request): Response
+    {
+        /** Parti formulaire de contact */
+
+        $contact = $this->createForm(ContactType::class);
+        $contact->handleRequest($request);
+
+        if ($contact->isSubmitted() && $contact->isValid()) {
+            $mail = (new Email())
+                ->from($contact->get('email')->getData())
+                ->to(new Address('johankasripro67@gmail.com', 'Johan Kasri'))
+                ->text($contact->get('content')->getData());
+            $mailer->send($mail);
+
+            $this->addFlash('message', 'Votre demande à bien été prise en compte ! ');
+        }
         return $this->render('home/index.html.twig', [
 
+        'contact'=>$contact->createView(),
         ]);
     }
 }
